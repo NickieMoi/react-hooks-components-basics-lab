efd35d1893980d9a4b34c314579abd55af770661
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-07-13 18:28:49 +0300

    lab complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 function NavBar() {
   return (
     <nav>
@@ -7,7 +6,6 @@ function NavBar() {
     </nav>
   );
 }
-
 function Home() {
   return (
     <div id="home">
@@ -17,6 +15,13 @@ function Home() {
 }
 
 {/* write an <About> component here */}
+function About(){
+  return(
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  )
+}
 
 
 function App() {
@@ -25,8 +30,9 @@ function App() {
       <NavBar />
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
+      <Home/> 
+      <About/>
     </div>
   );
 }
-
 export default App;
